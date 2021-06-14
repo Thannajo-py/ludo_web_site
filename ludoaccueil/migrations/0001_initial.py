77107bf5ad184c2b2c35a964c1c2fd4f0741e1ac
Thannajo-py <wolgan_4@hotmail.com>
2021-06-14 15:54:19 +0200

    add migration

--- ludoaccueil/migrations/0001_initial.py
+++ ludoaccueil/migrations/0001_initial.py
@@ -35,21 +35,4 @@ class Migration(migrations.Migration):
                 'verbose_name_plural': 'articles',
             },
         ),
-        migrations.CreateModel(
-            name='Comment',
-            fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('title', models.CharField(blank=True, max_length=200, null=True, verbose_name='titre')),
-                ('text_content', models.TextField(blank=True, null=True, verbose_name='commentaire')),
-                ('created_at', models.DateTimeField(auto_now_add=True, verbose_name='date de création')),
-                ('add_on', models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, to='ludorecherche.addon', verbose_name='extension')),
-                ('author', models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.DO_NOTHING, to=settings.AUTH_USER_MODEL, verbose_name='auteur')),
-                ('game', models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, to='ludorecherche.game', verbose_name='jeu')),
-                ('multi_add_on', models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, to='ludorecherche.multiaddon', verbose_name='extension partagée')),
-                ('news', models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, to='ludoaccueil.news', verbose_name='nouvelles')),
-            ],
-            options={
-                'verbose_name': 'commentaire',
-            },
-        ),
     ]
