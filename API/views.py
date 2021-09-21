1ec497572640d993f1e05f869a38f3e2ef354c58
Thannajo-py <wolgan_4@hotmail.com>
2021-09-21 11:00:18 +0200

    api fully working before heroku deployment

--- API/views.py
+++ API/views.py
@@ -36,38 +36,40 @@ def make_sub_dic(game, subDic):
     return this_dic
 
 
-@api_view(['GET'])
-def get_all(request):
-    if request.method == 'GET':
-        games = [game for game in Game.objects.order_by('name')]
-        multi_add_ons = [add_on for add_on in MultiAddOn.objects.order_by('name')]
-        add_ons = [add_on for add_on in AddOn.objects.order_by('name')]
-        dic_all = {'games': [
-            make_sub_dic(game, {
-             'by_player': game.by_player,
-             'tags': [tag.name for tag in game.tag.order_by('name')],
-             'topics': [topic.name for topic in game.topic.order_by('name')],
-             'mechanism': [mechanism.name for mechanism in game.mechanism.order_by('name')],
-             'add_on':
-                 [make_sub_dic(add_on, {
-                   'game': add_on.game.name
-                   }) for add_on in AddOn.objects.filter(game_id=game.pk)],
-             'multi_add_on': [make_sub_dic(multi_add_on, {
-                               'games': [source_game.name for source_game in multi_add_on.games.order_by('name')],
-                               }) for multi_add_on in MultiAddOn.objects.filter(games=game.pk)]
-             }) for game in games],
-        'multi_add_ons':[make_sub_dic(add_on, {
-                   'games': [game.name for game in add_on.games.order_by('name')]
-                   }) for add_on in multi_add_ons],
-            'add_ons':[make_sub_dic(add_on, {'game': add_on.game.name}) for add_on in add_ons if add_on.game is not None]
-        }
-        return Response(dic_all)
+def get_all():
+    games = [game for game in Game.objects.order_by('name')]
+    multi_add_ons = [add_on for add_on in MultiAddOn.objects.order_by('name')]
+    add_ons = [add_on for add_on in AddOn.objects.order_by('name')]
+    dic_all = {'games': [
+        make_sub_dic(game, {
+         'by_player': game.by_player,
+         'tags': [tag.name for tag in game.tag.order_by('name')],
+         'topics': [topic.name for topic in game.topic.order_by('name')],
+         'mechanism': [mechanism.name for mechanism in game.mechanism.order_by('name')],
+         'add_on':
+             [make_sub_dic(add_on, {
+               'game': add_on.game.name
+               }) for add_on in AddOn.objects.filter(game_id=game.pk)],
+         'multi_add_on': [make_sub_dic(multi_add_on, {
+                           'games': [source_game.name for source_game in multi_add_on.games.order_by('name')],
+                           }) for multi_add_on in MultiAddOn.objects.filter(games=game.pk)]
+         }) for game in games],
+    'multi_add_ons':[make_sub_dic(add_on, {
+               'games': [game.name for game in add_on.games.order_by('name')]
+               }) for add_on in multi_add_ons],
+        'add_ons':[make_sub_dic(add_on, {'game': add_on.game.name}) for add_on in add_ons if add_on.game is not None]
+    }
+    return dic_all
+
+
 def game_type(): return 'game'
 def add_on_type(): return 'add_on'
 def multi_add_on_type(): return 'multi_add_on'
 
+
 def get_int(content):
     return content if type(content) == int else None
+
 
 def common_field_fill(db_class, added_content, field, type_object):
     if (added_content):
@@ -90,22 +92,44 @@ def common_field_fill(db_class, added_content, field, type_object):
                         stock=get_int(game.get('stock', None)),
                         buying_price=get_int(game.get('buying_price', None))
                     )
-                    fill_link(game.get('designers'), Designer, new_game.designers)
-                    fill_link(game.get('artists'), Artist, new_game.artists)
-                    fill_link(game.get('publishers'), Publisher, new_game.publishers)
-                    fill_link(game.get('language'), Language, new_game.language)
-                    fill_link(game.get('playing_mode'), PlayingMode, new_game.playing_mode)
-                    if game.difficulty:
-                        try:
-                            new_game.difficulty = Difficulty.objects.get(name=game.get('difficulty'))
-                        except ObjectDoesNotExist:
-                            new_game.difficulty = Difficulty.objects.create(name=game.get('difficulty'))
-                    if type_object == game_type():
-                        new_game.by_player = game.get('by_player') if type(game.get('by_player') == bool) else False
-                        fill_link(game.get('tags'), Tag, new_game.tag)
-                        fill_link(game.get('mechanism'), Mechanism, new_game.mechanism)
-                        fill_link(game.get('topics'), Topic, new_game.topic)
+                    common_object_fill(game, new_game, type_object)
                     new_game.save()
+
+
+def common_object_fill(game, new_game, type_object):
+    fill_link(game.get('designers'), Designer, new_game.designers)
+    fill_link(game.get('artists'), Artist, new_game.artists)
+    fill_link(game.get('publishers'), Publisher, new_game.publishers)
+    fill_link(game.get('language'), Language, new_game.language)
+    fill_link(game.get('playing_mode'), PlayingMode, new_game.playing_mode)
+    if game.get('difficulty'):
+        try:
+            new_game.difficulty = Difficulty.objects.get(name=game.get('difficulty'))
+        except ObjectDoesNotExist:
+            new_game.difficulty = Difficulty.objects.create(name=game.get('difficulty'))
+    if type_object == game_type():
+        new_game.by_player = game.get('by_player') if type(game.get('by_player') == bool) else False
+        fill_link(game.get('tags'), Tag, new_game.tag)
+        fill_link(game.get('mechanism'), Mechanism, new_game.mechanism)
+        fill_link(game.get('topics'), Topic, new_game.topic)
+        new_game.save()
+
+
+def late_common_object_fill(content):
+    add_ons = content.get('add_ons')
+    if type(add_ons) == list:
+        for add_on in add_ons:
+            db_add_on = AddOn.objects.get(name=add_on.get('name'))
+            try:
+                db_add_on.game = Game.objects.get(name=add_on.get('game'))
+                db_add_on.save()
+            except ObjectDoesNotExist:
+                pass
+    multi_add_ons = content.get('multi_add_ons')
+    if type(multi_add_ons) == list:
+        for multi_add_on in multi_add_ons:
+            db_multi_add_on = MultiAddOn.objects.get(name=multi_add_on.get('name'))
+            fill_complex_element(multi_add_on.get('games'), Game, db_multi_add_on)
 
 
 def fill_link(list_type, db_class_type, db_object):
@@ -117,6 +141,7 @@ def fill_link(list_type, db_class_type, db_object):
             except ObjectDoesNotExist:
                 new_element = db_class_type.objects.create(name=element)
                 db_object.add(new_element)
+
 
 def fill_complex_element(list_type, db_class_type, db_object):
     if type(list_type) == list:
@@ -133,23 +158,54 @@ def add_dispatch(added_content):
     common_field_fill(Game, added_content, 'games', game_type())
     common_field_fill(AddOn, added_content, 'add_ons', add_on_type())
     common_field_fill(MultiAddOn, added_content, 'multi_add_ons', multi_add_on_type())
-    add_ons = added_content.get('add_ons')
-    if type(add_ons) == list:
-        for add_on in add_ons:
-            db_add_on = AddOn.objects.get(name=add_on.get('name'))
+    late_common_object_fill(added_content)
+
+
+def common_field_change(db_object, new_object:dict, type_object):
+    db_object.name = new_object.get('name')
+    db_object.english_name = new_object.get('english_name')
+    db_object.player_min = get_int(new_object.get('player_min'))
+    db_object.player_max = get_int(new_object.get('player_max'))
+    db_object.playing_time = new_object.get('playing_time')
+    db_object.bgg_link = new_object.get('bgg_link')
+    db_object.age = new_object.get('age')
+    db_object.buying_price = get_int(new_object.get('buying_price'))
+    db_object.stock = get_int(new_object.get('stock'))
+    db_object.max_time = get_int(new_object.get('max_time'))
+    common_object_fill(new_object, db_object, type_object)
+
+
+def list_object_from_dict(list_object, db_class, type_object):
+    if type(list_object) == list:
+        for data_object in list_object:
             try:
-                db_add_on.game = Game.objects.get(name=add_on.get('game'))
-                db_add_on.save()
+                db_data = db_class.objects.get(pk=data_object.get('id'))
+                common_field_change(db_data, data_object, type_object)
             except ObjectDoesNotExist:
-                pass
-    multi_add_ons = added_content.get('multi_add_ons')
-    if type(multi_add_ons) == list:
-        for multi_add_on in multi_add_ons:
-            db_multi_add_on = MultiAddOn.objects.get(name=multi_add_on.get('name'))
-            fill_complex_element(multi_add_on.get('games'), Game, db_multi_add_on)
+                continue
 
 
+def modify_dispatch(modified_content:dict):
+    list_object_from_dict(modified_content.get('games'), Game, game_type())
+    list_object_from_dict(modified_content.get('add_ons'), AddOn, add_on_type())
+    list_object_from_dict(modified_content.get('multi_add_ons'), MultiAddOn, multi_add_on_type())
+    late_common_object_fill(modified_content)
 
+
+def delete_from_db(list_object, db_class):
+    if type(list_object) == list:
+        for data in list_object:
+            try:
+                db_data = db_class.objects.get(pk=data.get('id'))
+                db_data.delete()
+            except ObjectDoesNotExist:
+                continue
+
+
+def delete_dispatch(deleted_content: dict):
+    delete_from_db(deleted_content.get('games'), Game)
+    delete_from_db(deleted_content.get('add_ons'), AddOn)
+    delete_from_db(deleted_content.get('multi_add_ons'), MultiAddOn)
 
 
 
@@ -158,19 +214,22 @@ def add_dispatch(added_content):
 def synchronize_change(request):
     if request.method == "POST":
         body = json.loads(request.POST.get('body'))
-        print(body)
         username = body.get('login')
         password = body.get('password')
         user = authenticate(request, username=username, password=password)
         if user is not None:
-           added_content = body.get('addedList')
-           add_dispatch(added_content)
-           modified_content = body.get('modifiedList')
-           deleted_content = body.get('deletedList')
-        raise UnicodeEncodeError
+            added_content = body.get('addedList')
+            if type(added_content) == dict:
+                add_dispatch(added_content)
+            modified_content = body.get('modifiedList')
+            if type(modified_content) == dict:
+                modify_dispatch(modified_content)
+            deleted_content = body.get('deletedList')
+            if type(deleted_content) == dict:
+                delete_dispatch(deleted_content)
+            return Response(get_all())
+        else:
+            return Response({'error': 'wrong credential'})
 
-
-
-    return Response({})
 
 
