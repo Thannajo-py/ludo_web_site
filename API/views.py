b80a70cfae598c226c0abfccdc52f36929c17c7c
Thannajo-py <wolgan_4@hotmail.com>
2022-01-08 15:24:29 +0100

    with encrypt password function

--- API/views.py
+++ API/views.py
@@ -14,9 +14,8 @@ from ludorecherche.models import Game, AddOn, MultiAddOn, Designer, Artist, Publ
 from .constant import game_type, add_on_type, multi_add_on_type
 
 
-
-
 def make_sub_dic(game, sub_dic):
+    """handle Game, AddOn, MultiAddOn common field dict"""
     this_dic = {
                 'id': game.pk,
                 'name': game.name,
@@ -43,6 +42,7 @@ def make_sub_dic(game, sub_dic):
 
 
 def get_all(timestamp, new_timestamp):
+    """Send the whole database in a JSON format"""
     games = [game for game in Game.objects.order_by('name')]
     multi_add_ons = [add_on for add_on in MultiAddOn.objects.order_by('name')]
     add_ons = [add_on for add_on in AddOn.objects.order_by('name')]
@@ -65,28 +65,55 @@ def get_all(timestamp, new_timestamp):
 
 
 def get_last_change(timestamp, new_timestamp):
+    """send only changed created or deleted element since timestamp"""
     games = [game for game in Game.objects.filter(modified_at__gte=timestamp).order_by('name')]
     multi_add_ons = [add_on for add_on in MultiAddOn.objects.filter(modified_at__gt=timestamp).order_by('name')]
     add_ons = [add_on for add_on in AddOn.objects.filter(modified_at__gt=timestamp).order_by('name')]
-    deleted_games = [game for game in DeletedGames.objects.filter(created_at__gt=timestamp,
-                                                                  product_type=game_type())]
-    deleted_add_ons = [add_on for add_on in DeletedGames.objects.filter(created_at__gt=timestamp,
-                                                                        product_type=add_on_type())]
-    deleted_multi_add_ons = [multi_add_on for multi_add_on in DeletedGames.objects.filter(
-        created_at__gt=timestamp, product_type=multi_add_on_type())]
+    deleted_games = [
+        game for game in DeletedGames.objects.filter(
+            created_at__gt=timestamp,
+            product_type=game_type()
+        )
+    ]
+    deleted_add_ons = [
+        add_on for add_on in DeletedGames.objects.filter(
+            created_at__gt=timestamp,
+            product_type=add_on_type()
+        )
+    ]
+    deleted_multi_add_ons = [
+        multi_add_on for multi_add_on in DeletedGames.objects.filter(
+        created_at__gt=timestamp,
+        product_type=multi_add_on_type()
+        )
+    ]
     dic_all = {'games': [
-        make_sub_dic(game, {
-         'by_player': game.by_player,
-         'tag': [tag.name for tag in game.tag.order_by('name')],
-         'topic': [topic.name for topic in game.topic.order_by('name')],
-         'mechanism': [mechanism.name for mechanism in game.mechanism.order_by('name')],
-         'add_on': [add_on.name for add_on in AddOn.objects.filter(game_id=game.pk)],
-         'multi_add_on': [multi_add_on.name for multi_add_on in MultiAddOn.objects.filter(games=game.pk)]
-         }) for game in games],
-        'multi_add_ons': [make_sub_dic(add_on, {
+        make_sub_dic(
+            game,
+            {
+             'by_player': game.by_player,
+             'tag': [tag.name for tag in game.tag.order_by('name')],
+             'topic': [topic.name for topic in game.topic.order_by('name')],
+             'mechanism': [mechanism.name for mechanism in game.mechanism.order_by('name')],
+             'add_on': [add_on.name for add_on in AddOn.objects.filter(game_id=game.pk)],
+             'multi_add_on': [
+                 multi_add_on.name for multi_add_on in MultiAddOn.objects.filter(games=game.pk)]
+             }
+        ) for game in games],
+        'multi_add_ons': [
+            make_sub_dic(
+                add_on,
+                {
                'games': [game.name for game in add_on.games.order_by('name')]
-               }) for add_on in multi_add_ons],
-        'add_ons': [make_sub_dic(add_on, {'game': add_on.game.name}) for add_on in add_ons if add_on.game is not None],
+               }
+            ) for add_on in multi_add_ons
+        ],
+        'add_ons': [
+            make_sub_dic(
+                add_on,
+                {'game': add_on.game.name}
+            ) for add_on in add_ons if add_on.game is not None
+        ],
         'deleted_games': [game.deleted_id for game in deleted_games],
         'deleted_add_ons': [game.deleted_id for game in deleted_add_ons],
         'deleted_multi_add_ons': [game.deleted_id for game in deleted_multi_add_ons],
@@ -96,10 +123,12 @@ def get_last_change(timestamp, new_timestamp):
 
 
 def get_int(content):
+    """return an int or none from a data"""
     return content if type(content) == int else None
 
 
 def common_field_fill(db_class, added_content, field, type_object):
+    """make common simple change for Game AddOn MultiAddOn"""
     if added_content:
         games = added_content.get(field)
         if type(games) == list:
@@ -126,6 +155,7 @@ def common_field_fill(db_class, added_content, field, type_object):
 
 
 def common_object_fill(game, new_game, type_object):
+    """make common change with link type"""
     fill_link(game.get('designer'), Designer, new_game.designers)
     fill_link(game.get('artist'), Artist, new_game.artists)
     fill_link(game.get('publisher'), Publisher, new_game.publishers)
@@ -145,6 +175,7 @@ def common_object_fill(game, new_game, type_object):
 
 
 def late_common_object_fill(content):
+    """handle linking after changed have been made"""
     add_ons = content.get('add_ons')
     if type(add_ons) == list:
         for add_on in add_ons:
@@ -162,6 +193,7 @@ def late_common_object_fill(content):
 
 
 def fill_link(list_type, db_class_type, db_object):
+    """handle many-to-many relationship create element if not present"""
     if type(list_type) == list:
         db_object.clear()
         for element in list_type:
@@ -174,6 +206,7 @@ def fill_link(list_type, db_class_type, db_object):
 
 
 def fill_complex_element(list_type, db_class_type, db_object):
+    """handle Game MultiAddOn many to many relationship"""
     if type(list_type) == list:
         db_object.clear()
         for element in list_type:
@@ -186,12 +219,14 @@ def fill_complex_element(list_type, db_class_type, db_object):
 
 
 def add_dispatch(added_content):
+    """handle add request"""
     common_field_fill(Game, added_content, 'games', game_type())
     common_field_fill(AddOn, added_content, 'add_ons', add_on_type())
     common_field_fill(MultiAddOn, added_content, 'multi_add_ons', multi_add_on_type())
 
 
 def common_field_change(db_object, new_object: dict, type_object):
+    """handle game add-on multi-add-on common field change"""
     db_object.name = new_object.get('name')
     db_object.english_name = new_object.get('english_name')
     db_object.player_min = get_int(new_object.get('player_min'))
@@ -207,6 +242,7 @@ def common_field_change(db_object, new_object: dict, type_object):
 
 
 def list_object_from_dict(list_object, db_class, type_object):
+    """check if object exist and new name is available(unique name db constraint)"""
     if type(list_object) == list:
         for data_object in list_object:
             if type(data_object) == dict:
@@ -223,12 +259,14 @@ def list_object_from_dict(list_object, db_class, type_object):
 
 
 def modify_dispatch(modified_content: dict):
+    """handle modification request"""
     list_object_from_dict(modified_content.get('games'), Game, game_type())
     list_object_from_dict(modified_content.get('add_ons'), AddOn, add_on_type())
     list_object_from_dict(modified_content.get('multi_add_ons'), MultiAddOn, multi_add_on_type())
 
 
 def delete_from_db(list_object, db_class):
+    """Delete object from database"""
     if type(list_object) == list:
         for data in list_object:
             if type(data) == dict:
@@ -240,16 +278,29 @@ def delete_from_db(list_object, db_class):
 
 
 def delete_dispatch(deleted_content: dict):
+    """Handle deletion request"""
     delete_from_db(deleted_content.get('games'), Game)
     delete_from_db(deleted_content.get('add_ons'), AddOn)
     delete_from_db(deleted_content.get('multi_add_ons'), MultiAddOn)
 
+
 def django_authentify(request, body):
-    username = body.get('login')
-    password = body.get('password')
+    """check if request credentials are correct and return the user"""
+    username = decrypt(body.get('login'))
+    password = decrypt(body.get('password'))
     return authenticate(request, username=username, password=password)
 
+
+def decrypt(text):
+    secret_key = "L66Fyiqhc49VzYthTsSGhmZlxnUz-S3YYBEz39o3_eE"
+    result = ""
+    for i in range(len(text)):
+        result += chr(text[i] - ord(secret_key[i % len(secret_key)]))
+    return result
+
+
 def modify_db(body):
+    """modify the database with a request body"""
     deleted_content = body.get('deletedList')
     if type(deleted_content) == dict:
         delete_dispatch(deleted_content)
@@ -270,6 +321,8 @@ def modify_db(body):
 @api_view(['POST'])
 @transaction.atomic
 def synchronize_change(request):
+    """With a POST request check user identification, permission and request body format then make change to database
+    and send back a JSON response with the new update(based on timestamp)"""
     if request.method == "POST":
         body_response = json.loads(request.body)
         if type(body_response) == dict:
